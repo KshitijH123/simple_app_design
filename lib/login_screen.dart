6560cbd75e5b284a46dc90558435bbbd62ff4063
Kshitij <kshitijhapase@gmail.com>
2024-05-17 16:29:47 +0530

    commit

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,10 +1,10 @@
 import 'dart:ffi';
+import 'dart:html';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-
 
 class LoginScreen extends StatelessWidget {
   final TextEditingController emailController = TextEditingController();
@@ -13,91 +13,86 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Login page'),
-
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              child: FittedBox(
-                child: CircleAvatar(
-                  backgroundImage: AssetImage(
-                    'assets/images/image1.png'
-                  ),
-                  radius: 120,
-                  
-                ),
-               
+      appBar: AppBar(
+        title: Text('Login page'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            child: FittedBox(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/images/image1.png'),
+                radius: 100,
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 30),
-              child: Form(
-                  child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 15),
-                    child: TextFormField(
-                        controller: emailController,
-                        keyboardType: TextInputType.emailAddress,
-                        decoration: InputDecoration(
-                          labelText: 'Email',
-                          hintText: 'Enter email',
-                          prefixIcon: Icon(Icons.email),
-                          border: OutlineInputBorder(),
-                        ),
-                        onChanged: (String value) {},
-                        validator: (value) {
-                          return value!.isEmpty ? 'please enter value' : null;
-                        }),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 30),
+            child: Form(
+                child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  child: TextFormField(
+                      controller: emailController,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        hintText: 'Enter email',
+                        prefixIcon: Icon(Icons.email),
+                        border: OutlineInputBorder(),
+                      ),
+                      onChanged: (String value) {},
+                      validator: (value) {
+                        return value!.isEmpty ? 'please enter value' : null;
+                      }),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  child: TextFormField(
+                      controller: passwordController,
+                      keyboardType: TextInputType.visiblePassword,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        hintText: 'Enter password',
+                        prefixIcon: Icon(Icons.password),
+                        border: OutlineInputBorder(),
+                      ),
+                      onChanged: (String value) {},
+                      validator: (value) {
+                        return value!.isEmpty ? 'please enter password' : null;
+                      }),
+                ),
+                SizedBox(height: 8),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 48),
+                  child: MaterialButton(
+                    onPressed: () {
+                      onLoginButtonPressed(context);
+                    },
+                    child: Text('Login',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 24,
+                        )),
+                    color: Colors.blue,
+                    textColor: Colors.white,
                   ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 15),
-                    child: TextFormField(
-                        controller: passwordController,
-                        keyboardType: TextInputType.visiblePassword,
-                        decoration: InputDecoration(
-                          labelText: 'Password',
-                          hintText: 'Enter password',
-                          prefixIcon: Icon(Icons.password),
-                          border: OutlineInputBorder(),
-                        ),
-                        onChanged: (String value) {},
-                        validator: (value) {
-                          return value!.isEmpty
-                              ? 'please enter password'
-                              : null;
-                        }),
-                  ),
-                  SizedBox(
-                    height: 8,
-                  ),
-                  
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 48),
-                    child: MaterialButton(
-                      onPressed: () {
-                        onLoginButtonPressed(context);
-                        
-                      },
-                      child: Text('Login'),
-                      color: Colors.blue,
-                      textColor: Colors.white,
-                    ),
-                  ),
-                   Text('Forgot Password ?')
-                ],
-              )),
-            )
-          ],
-        ),
-      );
+                ),
+                Text(
+                  'Forgot Password ?',
+                )
+              ],
+            )),
+          )
+        ],
+      ),
+    );
   }
 
   final email = 'kshitijhapase@gmail.com';
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,13 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:simple_app_design/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
-  
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
- 
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,9 +16,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
       ),
-      home:  LoginScreen(),
+      home: LoginScreen(),
     );
-
-    
   }
 }
+
+
